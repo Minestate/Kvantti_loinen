f32a24da2392daa8cb04e1e6e5a4e9f820a158eb
Nartti <niminen8@gmail.com>
2026-08-19 00:27:20 +0300

    Lisätty Chat_boxing tiedostot

--- server.js
+++ server.js
@@ -6,15 +6,14 @@ const app = express();
 const server = http.createServer(app);
 const io = new Server(server);
 
-// Tarjotaan staattiset tiedostot (kuten index.html) samasta kansiosta
+// Tarjotaan staattiset tiedostot samasta kansiosta
 app.use(express.static(__dirname));
 
 io.on('connection', (socket) => {
   console.log('QEMU-solmu yhdistetty socketin kautta:', socket.id);
 
-  // Välitetään salattu/kryptattu viesti eteenpäin kaikille solmuille
+  // Välitetään salattu viesti eteenpäin kaikille solmuille
   socket.on('chat message', (kryptattuViesti) => {
-    // Varmistetaan viestin muoto ja koko
     if (typeof kryptattuViesti === 'string' && kryptattuViesti.length <= 10000) {
       io.emit('chat message', kryptattuViesti);
     }
